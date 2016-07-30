ab53381bd3db921a393749c80c798225a4ca5535
Ramzy Karam <Ramzy.karam92@gmail.com>
2016-07-31 01:02:10 +0200

    final according to basic tutorial

--- src/components/Track.js
+++ src/components/Track.js
@@ -2,10 +2,11 @@ import React, {Component} from 'react';
 import { PlayButton, Progress, Timer } from 'react-soundplayer/components';
 
 export default class Track extends Component {
-  render(){
+  render() {
     const { track, soundCloudAudio, playing, seeking, currentTime, duration } = this.props;
     const currentProgress = currentTime / duration * 100 || 0;
-    return(
+
+    return (
       <div className="player">
         <PlayButton
           className="orange-button"
@@ -17,16 +18,16 @@ export default class Track extends Component {
           className="timer"
           soundCloudAudio={soundCloudAudio}
           currentTime={currentTime} />
-          <div className "track-info">
-            <h2 className="track-title">{track && track.title}</h2>
-            <h3 className="track-user">{track && track.user && track.user.username}</h3>
-          </div>
-          <Progress
-            className="progress-container"
-            innerClassName="progress"
-            soundCloudAudio={soundCloudAudio}
-            value={currentProgress} />
-          </div>
-        );
+        <div className="track-info">
+          <h2 className="track-title">{track && track.title}</h2>
+          <h3 className="track-user">{track && track.user && track.user.username}</h3>
+        </div>
+        <Progress
+          className="progress-container"
+          innerClassName="progress"
+          soundCloudAudio={soundCloudAudio}
+          value={currentProgress} />
+      </div>
+    );
   }
 }
